0ac9ccf68777cbf902a01ca44ed3b39d9698d71c
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-12-05 07:07:53 +0100

    Cleanup before rdm-14

--- oarepo_oaipmh_harvester/oai_record/views.py
+++
@@ -1,7 +0,0 @@
-def create_api_blueprint(app):
-    """Create OaiHarvesterRecord blueprint."""
-    blueprint = app.extensions[
-        "oarepo_oaipmh_harvester"
-    ].oai_record_resource.as_blueprint()
-
-    return blueprint
